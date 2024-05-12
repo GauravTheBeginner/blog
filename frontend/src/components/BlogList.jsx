b2f0b7b113890f89418a1a4562c6aff890307533
GauravTheBeginner <gaurav8861r@gmail.com>
2024-05-12 17:43:19 +0530

    added for python

--- frontend/src/components/BlogList.jsx
+++ frontend/src/components/BlogList.jsx
@@ -6,9 +6,10 @@ function BlogList() {
     useEffect(() => {
         const fetchBlogs = async () => {
             try {
-                const response = await fetch('http://localhost:8000/user/blogs/');
+                const response = await fetch('http://127.0.0.1:8000/api/blogposts/');
                 
                 const data = await response.json();
+                console.log(data)
                 setBlogs(data);
             } catch (error) {
                 console.error('Error fetching blogs:', error);
@@ -19,11 +20,11 @@ function BlogList() {
     }, []);
     const handleDelete = async (id) => {
         try {
-            await fetch(`http://localhost:8000/user/blogs/${id}`, {
+            await fetch(`http://127.0.0.1:8000/api/blogposts/?id=${id}`, {
                 method: 'DELETE'
             });
            
-            setBlogs(prevBlogs => prevBlogs.filter(blog => blog._id !== id));
+            setBlogs(prevBlogs => prevBlogs.filter(blog => blog.id !== id));
         } catch (error) {
             console.error('Error deleting blog:', error);
         }
@@ -41,15 +42,15 @@ function BlogList() {
         <div className="flex justify-center px-4 py-9 flex-wrap gap-7  items-center">
             {blogs.map((item, index) => (
                 <div className="border border-black rounded overflow-hidden shadow-lg w-80" key={index}>
-                    <img className="w-full h-48 object-cover" src={item.image} alt={item.title} />
+                    {/* <img className="w-full h-48 object-cover" src={item.image} alt={item.title} /> */}
                     <div className="px-6 py-4">
                         <div className="font-bold truncate text-xl mb-2">{item.title}</div>
-                        <p className="text-gray-700 truncate text-base">{item.description}</p>
+                        <p className="text-gray-700 truncate text-base">{item.content}</p>
                     </div>
                     <div className="px-6 py-4 flex gap-2">
-                        <button onClick={()=> handleUpdate(item._id)} className="border-2 px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600">Update</button>
-                        <button onClick={() => handleRead(item._id)} className="border-2 px-4 py-2 rounded-md bg-green-500 text-white hover:bg-green-600">Read</button>
-                        <button  onClick={() => handleDelete(item._id)} className="border-2 px-4 py-2 rounded-md bg-red-500 text-white hover:bg-red-600">Delete</button>
+                        <button onClick={()=> handleUpdate(item.id)} className="border-2 px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600">Update</button>
+                        <button onClick={() => handleRead(item.id)} className="border-2 px-4 py-2 rounded-md bg-green-500 text-white hover:bg-green-600">Read</button>
+                        <button  onClick={() => handleDelete(item.id)} className="border-2 px-4 py-2 rounded-md bg-red-500 text-white hover:bg-red-600">Delete</button>
                     </div>
                 </div>
             ))}
--- frontend/src/components/Update.jsx
+++ frontend/src/components/Update.jsx
@@ -3,36 +3,36 @@ import { useNavigate, useParams } from "react-router-dom";
 
 function Update() {
   const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [image, setImage] = useState("");
+  const [content, setcontent] = useState("");
+
   const navigate = useNavigate();
   const { id } = useParams();
   const updateData = async () => {
-    if (!title || !description || !image) {
+    if (!title || !content ) {
       alert("Please fill out all fields");
       return;
     }
     try {
-      const response = await fetch(`http://localhost:8000/user/blogs/${id}`, {
-        method: 'PUT',
+      const response = await fetch(`http://127.0.0.1:8000/api/blogposts/?id=${id}`, {
+        method: 'PATCH',
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
           title: title,
-          description: description,
-          image: image
+          content: content,
+          
         })
       });
-  
+      console.log(response)
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
   
    
       alert("upadated")
-      setDescription("")
-      setImage("")
+      setcontent("")
+     
       setTitle("")
       navigate('/blogs')
     } catch (error) {
@@ -58,21 +58,12 @@ function Update() {
         className="mt-1 mb-4 p-2 block w-full border rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500" 
       />
 
-      <label htmlFor="image" className="block text-gray-700 font-semibold">Image URL</label>
-      <input 
-        required 
-        onChange={(e) => setImage(e.target.value)} 
-        value={image} 
-        type="text" 
-        id="image" 
-        className="mt-1 mb-4 p-2 block w-full border rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500" 
-      />
-
+     
       <label htmlFor="content" className="block text-gray-700 font-semibold">Content</label>
       <textarea 
         required 
-        onChange={(e) => setDescription(e.target.value)} 
-        value={description} 
+        onChange={(e) => setcontent(e.target.value)} 
+        value={content} 
         id="content" 
         className="mt-1 mb-4 p-2 block w-full border rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500" 
         rows="5"
